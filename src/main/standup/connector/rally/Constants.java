cf8254c5c4768af229bbb1dffa06c08a901a505f
Dave Shawley <daveshawley@gmail.com>
2010-09-18 21:20:12 -0700

    Upgraded Rally schema from 1.17 to 1.21.  No real changes or functionality as far as I can tell.

--- src/main/standup/connector/rally/Constants.java
+++ src/main/standup/connector/rally/Constants.java
@@ -7,7 +7,7 @@ package standup.connector.rally;
  * Rally API and Server Constants.
  */
 public class Constants {
-	public final static String RALLY_API_VERSION = "1.17";
+	public final static String RALLY_API_VERSION = "1.21";
 	public final static String RALLY_BASE_RESOURCE = "/slm/webservice";
 	public final static String RALLY_SERVER_NAME = "rally1.rallydev.com";
 }
